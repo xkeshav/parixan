0bba37a15ca3ef76f52a99edd7ee9f8e7f2f1684
Keshav Mohta <xkeshav@gmail.com>
2022-12-10 16:05:18 +0530

    chrome extension updated

--- ad-skip/background.js
+++
@@ -1,8 +0,0 @@
-const extensionURL = `http://github.com/xkeshav`;
-
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.runtime.setUninstallURL(extensionURL);
-  }
-  console.log('Ad SKIP installed successfully');
-});
